69ae2f7132fd119ac898f4233aaf829797440cb0
ScorpionOrange <scorpionorange@126.com>
2019-12-30 00:12:57 +0800

    update at 2019.12.30

--- app/src/main/java/com/example/pfasinjava/MainActivity.java
+++ app/src/main/java/com/example/pfasinjava/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.pfasinjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnOK = null;
+    private EditText etInput = null;
+    private TextView tvOutput = null;
+    private String strEtInput = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
