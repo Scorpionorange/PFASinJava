030d9ed60b9e4a4dc39add800f2b437dd7b58000
ScorpionOrange <scorpionorange@126.com>
2019-12-30 00:20:14 +0800

    update at 2019.12.30

--- app/src/main/java/com/example/pfasinjava/MainActivity.java
+++ app/src/main/java/com/example/pfasinjava/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.pfasinjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnOK = null;
@@ -17,5 +20,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etInput = (EditText) findViewById(R.id.etFAInput);
+        btnOK = (Button) findViewById(R.id.btnFAOK);
+        tvOutput = (TextView) findViewById(R.id.tvFAOutput);
+        btnOK.setOnClickListener(new OnClickListener(){
+
+            @Override
+            //当按钮按下后：
+            public void onClick(View v) {
+                //把etInput内输入的值赋给strEtInput;
+                strEtInput = etInput.getText().toString();
+                //在tvOutput内输出etInput内输入的值;
+                tvOutput.setText(strEtInput);
+            }
+        });
     }
 }
